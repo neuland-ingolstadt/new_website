55604e6e0ec941b0300daee2a1186c1116ce29d8
maw4498 <magdalena.waldau@outlook.de>
2023-09-27 21:10:08 +0200

    Draggable window

--- rogue-app/src/component/HideObjekt.jsx
+++ rogue-app/src/component/HideObjekt.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import Sponsor from "../Sponsor";
+import Draggable from "react-draggable";
 
 export default function HideObjekt(){
     const [dp, setDp] = useState(true);
     return (
+        <Draggable>
         <div>
             <button type="button" onClick={() => setDp(!dp)}> X </button>
             {dp && <div><Sponsor/></div>}
         </div>
+        </Draggable>
     );
   }
   
